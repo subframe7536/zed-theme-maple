c923c1f5b2c53436b87e3c65041cf7d0419ec7f1
subframe7536 <1667077010@qq.com>
2025-08-06 18:54:44 +0800

    split files

---
+++ src/theme.ts
@@ -0,0 +1,176 @@
+import { buildTerminalColor } from '../vscode/src/terminal';
+import type { BaseColor, TokenColor, UIColor } from '../vscode/src/type';
+import { parseColor, getSchemeTextColor } from '../vscode/src/util';
+import { buildUI, buildSyntax } from './utils';
+
+export function buildTheme(
+  base: BaseColor,
+  token: TokenColor,
+  ui: UIColor,
+  isDark: boolean
+) {
+  return {
+    ...buildUI({
+      background: {
+        DEFAULT: ui.background
+      },
+      text: {
+        DEFAULT: ui.foreground,
+        accent: ui.secondary,
+        muted: parseColor(ui.foreground, 0.7)
+      },
+      hint: {
+        DEFAULT: ui.secondary
+      },
+      elevated_surface: {
+        background: ui.background
+      },
+      editor: {
+        foreground: ui.foreground,
+        background: ui.backgroundEditor,
+        gutter: {
+          background: ui.background,
+        },
+        document_highlight: {
+          bracket_background: parseColor(ui.cursor, isDark ? 0.5 : 0.3),
+        },
+        active_line: {
+          background: parseColor(ui.selection, 0.25),
+        },
+        active_line_number: getSchemeTextColor(ui.background),
+        line_number: parseColor(base.gray, 0.7),
+        highlighted_line: {
+          background: parseColor(ui.selection, 0.5),
+        },
+      },
+      status_bar: {
+        background: ui.background,
+      },
+      toolbar: {
+        background: parseColor(ui.backgroundEditorAlt, 0.5),
+      },
+      element: {
+        hover: parseColor(ui.listItem, 0.6),
+        active: parseColor(ui.listItem, 0.8),
+        selected: ui.listItem,
+      },
+      ghost_element: {
+        hover: parseColor(ui.listItem, 0.6),
+        active: parseColor(ui.listItem, 0.8),
+        selected: ui.listItem
+      },
+      terminal: {
+        background: ui.backgroundEditor,
+        foreground: ui.foreground,
+        ansi: buildTerminalColor(
+          base,
+          isDark,
+          (type, isBright) => isBright ? `bright_${type}` : type
+        ),
+      },
+      created: {
+        DEFAULT: token.diff.inserted,
+      },
+      deleted: {
+        DEFAULT: token.diff.deleted
+      },
+      modified: {
+        DEFAULT: token.diff.changed
+      },
+      conflict: {
+        DEFAULT: base.purple
+      },
+      renamed: {
+        DEFAULT: base.cyan
+      },
+      ignored: {
+        DEFAULT: base.gray
+      },
+      border: {
+        DEFAULT: ui.borderNormal,
+        focused: parseColor(ui.borderActive, 0.8),
+        selected: ui.borderActive,
+      },
+      panel: {
+        background: ui.background,
+      },
+
+      link_text: {
+        hover: ui.backgroundEditorAlt
+      },
+      scrollbar: {
+        thumb: {
+          background: parseColor(ui.scrollbar, isDark ? 0.5 : 0.3),
+          hover_background: parseColor(ui.scrollbar, isDark ? 0.8 : 0.6),
+        }
+      },
+      hidden: {
+        background: ui.backgroundEditorAlt
+      },
+      success: {
+        DEFAULT: base.green,
+        background: parseColor(base.green, 0.3),
+      },
+      info: {
+        DEFAULT: base.blue,
+        background: parseColor(base.blue, 0.3),
+      },
+      error: {
+        DEFAULT: base.red,
+        background: parseColor(base.red, 0.3),
+      },
+      warning: {
+        DEFAULT: base.yellow,
+        background: parseColor(base.yellow, 0.3)
+      },
+      unreachable: {
+        DEFAULT: base.gray,
+      },
+      players: [
+        {
+          cursor: ui.cursor,
+          background: ui.backgroundEditor,
+          selection: ui.selection,
+        },
+      ],
+    }),
+    syntax: buildSyntax({
+      attribute: token.property.normal,
+      boolean: {
+        color: token.boolean,
+        fontStyle: 'italic',
+      },
+      comment: token.comment,
+      constant: token.constant,
+      constructor: token.keyword.alt,
+      embedded: token.constant,
+      emphasis: {
+        color: token.link,
+        fontStyle: 'italic',
+      },
+      enum: token.enum.normal,
+      function: token.function,
+      hint: token.comment,
+      keyword: {
+        color: token.keyword.normal,
+        fontStyle: 'italic',
+      },
+      label: token.function,
+      namespace: token.namespace,
+      number: token.number,
+      operator: token.operator,
+      primary: token.builtin,
+      property: token.property.normal,
+      punctuation: token.punctuation,
+      string: token.string,
+      selector: token.property.normal,
+      'selector.pseudo': token.css.pseudo,
+      tag: token.htmlTag,
+      type: {
+        color: token.type.normal,
+        fontWeight: 700,
+      },
+      variable: token.variable.local,
+    }),
+  };
+}
---
+++ src/utils.ts
@@ -0,0 +1,77 @@
+import type { Prettify } from '@subframe7536/type-utils';
+import type { ThemeStyleContent, HighlightStyleContent } from './type';
+
+type GatherByPrefix<T, K extends string> = {
+  [P in keyof T as P extends `${K}.${infer Tail}` ? Tail : never]: T[P];
+};
+
+type HasDirectKey<T, K extends string> = K extends keyof T ? true : false;
+
+type HasNestedKey<T, K> = K extends string
+  ? Extract<keyof T, `${K}.${string}`> extends never
+    ? false
+    : true
+  : never;
+
+type TopLevelKeys<K> = K extends `${infer Head}.${string}` ? Head : K;
+type RemoveStringIndex<T> = {
+  [K in keyof T as string extends K ? never : K]: T[K];
+};
+
+type ExpandDotKeys<T extends object> = {
+  [K in TopLevelKeys<keyof T> & string]?: HasDirectKey<T, K> extends true
+    ? HasNestedKey<T, K> extends true
+      ? Prettify<ExpandDotKeys<GatherByPrefix<T, K>> & { DEFAULT?: T[K & keyof T] }>
+      : T[K & keyof T]
+    : ExpandDotKeys<GatherByPrefix<T, K>>;
+};
+
+type UI = Omit<ExpandDotKeys<RemoveStringIndex<ThemeStyleContent>>, 'syntax'>;
+
+export function buildUI(themeDev: UI) {
+  const theme: Record<string, any> = {};
+
+  function flatten(obj: any, prefix = '') {
+    for (const key in obj) {
+      const value = obj[key];
+      const newKey = prefix ? key === 'DEFAULT' ? prefix : `${prefix}.${key}` : key;
+
+      if (value && typeof value === 'object') {
+        if (Array.isArray(value)) {
+          theme[newKey] = value;
+        } else {
+          flatten(value, newKey);
+        }
+      } else {
+        theme[newKey] = value;
+      }
+    }
+  }
+
+  flatten(themeDev);
+  return theme;
+}
+
+interface SyntaxConfig {
+  color: string;
+  backgroundColor?: string;
+  fontStyle?: 'normal' | 'italic' | 'oblique';
+  fontWeight?: 100 | 200 | 300 | 400 | 500 | 600 | 700 | 800 | 900;
+}
+
+export function buildSyntax(obj: Record<string, string | SyntaxConfig>): Record<string, HighlightStyleContent> {
+  const result: Record<string, HighlightStyleContent> = {};
+  for (const [k, v] of Object.entries(obj)) {
+    if (typeof v === 'string') {
+      result[k] = { color: v };
+    } else if (typeof v === 'object') {
+      result[k] = {
+        color: v.color,
+        font_style: v.fontStyle,
+        font_weight: v.fontWeight,
+        background_color: v.backgroundColor,
+      };
+    }
+  }
+  return result;
+}
