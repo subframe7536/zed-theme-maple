8db00410944204eb317fc5594efa1fc93f035b0d
subframe7536 <1667077010@qq.com>
2025-08-17 17:34:04 +0800

    dynamic schema version

--- update.ts
+++ update.ts
@@ -1,6 +1,10 @@
 import { compile } from 'json-schema-to-typescript'
 
-fetch('https://zed.dev/schema/themes/v0.2.0.json')
-  .then(r => r.json())
-  .then(json => compile(json as any, ''))
-  .then(text => Bun.file('./src/type.ts').write(text))
+const version = process.argv[2] || 'v0.2.0'
+
+const text = await fetch(`https://zed.dev/schema/themes/${version}.json`).then(r => r.text())
+if (!text.startsWith('{')) {
+  console.log("No schema:", text.slice(0, 500))
+  process.exit(1)
+}
+Bun.file('./src/type.ts').write(await compile(JSON.parse(text), ''))
